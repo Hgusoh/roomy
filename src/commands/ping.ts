62394e5502d17084087d9dc8c3ae64678bfef402
Hugo Helluy <hugo.helluy@orange.com>
2026-03-07 18:34:08 +0100

    refactor: updated from feur ton pong ping response

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -5,5 +5,5 @@ export const data = new SlashCommandBuilder()
     .setDescription("Replies with Pong!");
 
 export async function execute(interaction: ChatInputCommandInteraction) {
-    return interaction.reply("Feur!");
+    return interaction.reply("Pong!");
 }
